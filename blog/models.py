87c46fcf00577c6349f6828c685fb38d1263e480
Alexander Rivas <123262411+AleRivasSora@users.noreply.github.com>
2023-09-29 12:31:53 -0400

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,4 @@ class Blog(models.Model):
     def get_absolute_url(self):
         return reverse('blog_detail', args = [str(self.id)])
 
-# i created this get_absolute_url so that after updation the user can get redirected to the
-#  detail page, not on the list page. 
-# Reason for creating this function is that, the blog_detail page needs blog id and we cannot
-# mention that inside submit button class neither with the class of views.py beacause doing so
-# leads to errors.  
+
